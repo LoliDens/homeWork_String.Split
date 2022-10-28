81b7f6bff7f5ce6a213b57ad82da2e2bc2a08b4e
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-28 19:22:55 +0500

    Добавьте файлы проекта.

---
+++ homeWork_String.Split/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork_String.Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                string line = "Данные были успешно удалены";
+                string[] lines = line.Split(' ');
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    Console.WriteLine(lines[i]);
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
