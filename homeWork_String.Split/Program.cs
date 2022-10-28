5a8670054f4bdace770ca4edd13bb72c4ac2ddca
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-28 19:23:23 +0500

    123

--- homeWork_String.Split/Program.cs
+++ homeWork_String.Split/Program.cs
@@ -10,18 +10,15 @@ namespace homeWork_String.Split
     {
         static void Main(string[] args)
         {
-            static void Main(string[] args)
+            string line = "Данные были успешно удалены";
+            string[] lines = line.Split(' ');
+
+            for (int i = 0; i < lines.Length; i++)
             {
-                string line = "Данные были успешно удалены";
-                string[] lines = line.Split(' ');
-
-                for (int i = 0; i < lines.Length; i++)
-                {
-                    Console.WriteLine(lines[i]);
-                }
-
-                Console.ReadKey();
+                Console.WriteLine(lines[i]);
             }
+
+            Console.ReadKey();
         }
     }
 }
